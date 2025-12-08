e69dbd74cac758d16aa07fc5b060e52352f01870
carlos <carlos@furbo.ch>
2025-12-08 14:35:47 +0100

    Webhook

--- src/controllers/WebhookController.php
+++ src/controllers/WebhookController.php
@@ -3,9 +3,12 @@
 namespace furbo\craftmailomat\controllers;
 
 use Craft;
+use craft\helpers\Json;
 use craft\web\Controller;
 use putyourlightson\campaign\Campaign;
+use yii\log\Logger;
 use yii\web\ForbiddenHttpException;
+use yii\web\MethodNotAllowedHttpException;
 use yii\web\Response;
 
 /**
@@ -19,7 +22,7 @@ class WebhookController extends \putyourlightson\campaign\controllers\WebhookCon
     public $enableCsrfValidation = false;
 
     protected int|bool|array $allowAnonymous = [
-        'mailomat',
+        'index',
     ];
 
     public function beforeAction($action): bool
@@ -35,10 +38,85 @@ class WebhookController extends \putyourlightson\campaign\controllers\WebhookCon
         return parent::beforeAction($action);
     }
 
+    /**
+     * @throws MethodNotAllowedHttpException
+     */
     public function actionIndex(): ?Response
     {
-        dd(1);
-        //$this->requirePostRequest();
+        $this->requirePostRequest();
+
+        $body = Json::decodeIfJson($this->request->getRawBody());
+
+        Craft::info("Received Mailomat webhook: " . print_r($body, true), __METHOD__);
+
+        if(is_array($body)) {
+            $eventType = $body['eventType'] ?? null;
+
+            if($eventType === 'complained') {
+                $email = $body['email'] ?? null;
+                return $this->callWebhook('complained', $email);
+            }
+            if($eventType === 'bounced') {
+                $email = $body['email'] ?? null;
+                return $this->callWebhook('bounced', $email);
+            }
+            if($eventType === 'unsubscribed') {
+                $email = $body['email'] ?? null;
+                return $this->callWebhook('unsubscribed', $email);
+            }
+
+            return $this->asRawSuccess();
+        }
+
+        return $this->asRawFailure('No event provided.');
+
+    }
+
+    /**
+     * Returns a raw response success.
+     */
+    private function asRawSuccess(string $message = ''): Response
+    {
+        return $this->asRaw(Craft::t('campaign', $message));
+    }
+
+    /**
+     * Returns a raw response failure.
+     */
+    private function asRawFailure(string $message = ''): Response
+    {
+        Campaign::$plugin->log($message, [], Logger::LEVEL_WARNING);
+
+        return $this->asRaw(Craft::t('campaign', $message))
+            ->setStatusCode(400);
+    }
+
+    /**
+     * Calls a webhook.
+     */
+    private function callWebhook(string $event, string $email = null): Response
+    {
+        Campaign::$plugin->log('Webhook request: ' . $this->request->getRawBody(), [], Logger::LEVEL_WARNING);
+
+        if ($email === null) {
+            return $this->asRawFailure('No email provided.');
+        }
+
+        $contact = Campaign::$plugin->contacts->getContactByEmail($email);
+
+        if ($contact === null) {
+            return $this->asRawSuccess();
+        }
+
+        if ($event === 'complained') {
+            Campaign::$plugin->webhook->complain($contact);
+        } elseif ($event === 'bounced') {
+            Campaign::$plugin->webhook->bounce($contact);
+        } elseif ($event === 'unsubscribed') {
+            Campaign::$plugin->webhook->unsubscribe($contact);
+        }
+
+        return $this->asRawSuccess();
     }
 
 }
